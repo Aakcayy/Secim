50c55e95cd9e49ad7be3fa754c3e1d1a17917213
ahmet <ahmetakcay944@gmail.com>
2024-02-06 13:36:56 +0300

    Kod
    FrmSecim formunda değişiklik yapıldı
    
    1)Oy girişi yapılmadan kaydet butonuna tıklanırsa uyarı verecek
    
    2)İl seçilmeden textboxlar pasif durumunda kalacak
    İl seçilince oy girişi için textboxlar açılacak
    
    3)Oylar sayıldıkça veri girişi yapılacak ve girildikçe güncellenecek.

--- Secim/FrmSecim.cs
+++ Secim/FrmSecim.cs
@@ -35,7 +35,11 @@ namespace Secim
             {
                 cbxIL.Items.Add(dr["IL"]);
             }
-         
+            tbxAPARTI.Enabled = false;
+            tbxBParti.Enabled = false;
+            tbxCParti.Enabled = false;
+            tbxDParti.Enabled = false;
+            tbxEParti.Enabled = false;
 
 
         }
@@ -62,11 +66,18 @@ namespace Secim
                 while (dt.Read())
                 {
                     cbxILCE.Items.Add(dt["ILCE"].ToString());
+
+                    
                 }
 
-              
+                tbxAPARTI.Enabled = true;
+                tbxBParti.Enabled = true;
+                tbxCParti.Enabled = true;
+                tbxDParti.Enabled = true;
+                tbxEParti.Enabled = true;
             }
            
+
         }
         private void btnSonuclar_Click(object sender, EventArgs e)
         {
@@ -80,16 +91,50 @@ namespace Secim
 
         private void btnKaydet_Click(object sender, EventArgs e)
         {
-            SqlCommand oy = new SqlCommand("Update TBLSECIM SET AParti=@p1,BParti=@p2,CParti=@p3,DParti=@p4,EParti=@p5 where IL=@p6 and ILCE=@p7", baglan.baglanti());
-            oy.Parameters.AddWithValue("@p1",tbxAPARTI.Text);
-            oy.Parameters.AddWithValue("@p2", tbxBParti.Text);
-            oy.Parameters.AddWithValue("@p3", tbxCParti.Text);
-            oy.Parameters.AddWithValue("@p4", tbxDParti.Text);
-            oy.Parameters.AddWithValue("@p5", tbxEParti.Text);
-            oy.Parameters.AddWithValue("@p6", cbxIL.SelectedItem.ToString());
-            oy.Parameters.AddWithValue("@p7", cbxILCE.SelectedItem.ToString());
-            oy.ExecuteNonQuery();
-            MessageBox.Show("Güncelleme yapılmıştır");
+            if (cbxILCE.SelectedItem == null && cbxIL.SelectedItem == null && tbxAPARTI.Text == "" && tbxBParti.Text == "" && tbxCParti.Text == "" && tbxDParti.Text == "" && tbxEParti.Text == "")
+            {
+                MessageBox.Show("Eksik veri girişi yapılmıştır.");
+            }
+            else 
+            {
+                SqlCommand guncelleme = new SqlCommand("SELECT AParti,BParti,CParti,DParti,EParti FROM TBLSECIM WHERE IL=@p1 and ILCE=@p2", baglan.baglanti());
+                guncelleme.Parameters.AddWithValue("@p1", cbxIL.SelectedItem.ToString());
+                guncelleme.Parameters.AddWithValue("@p2", cbxILCE.SelectedItem.ToString());
+                SqlDataReader dt = guncelleme.ExecuteReader();
+                while (dt.Read())
+                {
+                    
+                    int eskiA = Convert.ToInt32(dt["AParti"]);
+                    int eskiB = Convert.ToInt32(dt["BParti"]);
+                    int eskiC = Convert.ToInt32(dt["CParti"]);
+                    int eskiD = Convert.ToInt32(dt["DParti"]);
+                    int eskiE = Convert.ToInt32(dt["EParti"]);
+                    int yeniA = eskiA + Convert.ToInt32(tbxAPARTI.Text);
+                    int yeniB = eskiB + Convert.ToInt32(tbxBParti.Text);
+                    int yeniC = eskiC + Convert.ToInt32(tbxCParti.Text);
+                    int yeniD = eskiD + Convert.ToInt32(tbxDParti.Text);
+                    int yeniE = eskiE + Convert.ToInt32(tbxEParti.Text);
+
+                    SqlCommand oy = new SqlCommand("Update TBLSECIM SET AParti=@p1,BParti=@p2,CParti=@p3,DParti=@p4,EParti=@p5 where IL=@p6 and ILCE=@p7", baglan.baglanti());
+
+                    oy.Parameters.AddWithValue("@p1", yeniA);
+                    oy.Parameters.AddWithValue("@p2", yeniB);
+                    oy.Parameters.AddWithValue("@p3", yeniC);
+                    oy.Parameters.AddWithValue("@p4", yeniD);
+                    oy.Parameters.AddWithValue("@p5", yeniE);
+                    oy.Parameters.AddWithValue("@p6", cbxIL.SelectedItem.ToString());
+                    oy.Parameters.AddWithValue("@p7", cbxILCE.SelectedItem.ToString());
+                    oy.ExecuteNonQuery();
+                    MessageBox.Show("Güncelleme yapılmıştır");
+
+                }
+                tbxAPARTI.Text = "";
+                tbxBParti.Text = "";
+                tbxCParti.Text = "";
+                tbxDParti.Text = "";
+                tbxEParti.Text = "";
+             }
+
         }
     }
 }
